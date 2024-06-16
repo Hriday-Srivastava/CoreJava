214080b48e202d554b9bfeb3830df914d7d9251f
Hriday-Kumar <heartbihar@gmail.com>
2024-06-16 14:50:45 +0530

    Reverse String Using Recursion As Well

---
+++ LogicalPreparation_2024/src/logicals/ReverseOfString.java
@@ -0,0 +1,38 @@
+package logicals;
+
+public class ReverseOfString {
+
+	public static void main(String[] args) {
+		
+		//Logical way
+		String str = "DCBA";
+		String reverse = reverseString(str);
+		System.out.println("The reverse of "+str+" = "+reverse);
+
+		//Using recursion reverse the String.
+		System.out.println("Using RECURSION the reverse of "+str+" = "+recursionReverseString(str));
+	}
+	
+	public static String reverseString(String str) {
+
+		char[] charArray = new char[str.length()];
+
+		for(int i = charArray.length- 1, j = 0; i >= 0; i--) {
+
+			charArray[j] = str.charAt(i);
+			j++;
+		}
+		return new String(charArray);
+	}
+	
+	public static String recursionReverseString(String str) {
+
+		if(str.length() == 0)
+			return "";
+		else
+			return str.charAt(str.length() - 1) + str.substring(0,str.length() - 1);
+
+
+	}
+
+}
