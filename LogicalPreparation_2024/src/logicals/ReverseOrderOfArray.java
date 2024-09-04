fc4a982a13086fce3e944cccaaab37e440361d43
Hriday-Kumar <heartbihar@gmail.com>
2024-09-04 10:23:23 +0530

    Reverse the order of an array

--- LogicalPreparation_2024/src/logicals/ReverseOrderOfArray.java
+++ LogicalPreparation_2024/src/logicals/ReverseOrderOfArray.java
@@ -5,18 +5,21 @@ import java.util.Arrays;
 public class ReverseOrderOfArray {
 
 	public static void main(String[] args) {
-		
-		int[] intArray = {20,30,10,50,40,90,70};
-		System.out.println("Given Array is : "+Arrays.toString(intArray));
-		
+
+		// 1st Method
+		int[] intArray = { 20, 30, 10, 50, 40, 90, 70 };
+		System.out.println("Given Array is : " + Arrays.toString(intArray));
+
 		int temp;
-		for(int i=0, j=intArray.length-1; i<=j; i++,j--) {
-			
-			temp = intArray[i];
-			intArray[i] = intArray[j];
-			intArray[j] = temp;
+		for (int start = 0, end = intArray.length - 1; start < end; start++, end--) {
+
+			temp = intArray[start];
+			intArray[start] = intArray[end];
+			intArray[end] = temp;
+
 		}
-		System.out.println("The reverse order of above array is : "+Arrays.toString(intArray));
+
+		System.out.println("The reverse order of above array is : " + Arrays.toString(intArray));
 
 	}
 
