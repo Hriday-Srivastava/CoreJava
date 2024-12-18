9051e71406b1c0d8240fefb9ada1fc3e878ba1db
Hriday-Kumar <heartbihar@gmail.com>
2024-12-18 18:45:51 +0530

    Find longest prefix

---
+++ LogicalPreparation_2024/src/logicals/PrefixLongest.java
@@ -0,0 +1,30 @@
+package logicals;
+
+public class PrefixLongest {
+
+	public static void main(String[] args) {
+		String[] strs = {"flower", "flow", "flight", "flowest"};
+        System.out.println("Longest Common Prefix: " + findLongestCommonPrefix(strs));
+
+	}
+	
+	public static String findLongestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+
+        String prefix = strs[0];
+        for (int i = 1; i < strs.length; i++) {
+            while (strs[i].indexOf(prefix) != 0) { // The method returns the index of the first occurrence of the prefix. If the string doesn't contain the prefix, it returns -1.
+                prefix = prefix.substring(0, prefix.length() - 1);
+                if (prefix.isEmpty()) {
+                    return "";
+                }
+            }
+        }
+        return prefix;
+
+}
+	
+}
+
